23cf1a914217bb9a2a76dee41c9d4453e97877cc
AbdessamadTzn <abdessamadtouzani07@gmail.com>
2024-03-04 13:12:55 +0100

    Feat: Flight Crashes Dashboard using Dash

--- Analyzing Flight Crashes/FlightCrashes_Dash.py
+++ Analyzing Flight Crashes/FlightCrashes_Dash.py
@@ -8,13 +8,20 @@ flightCrashes_data = pd.read_csv('AbdessamadTzn_flightCrashes_cleanedData.csv', 
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    [
-        html.H1(children='Flight Crashes Visualization', style={'textAlign':'center'}),
-        html.Div('Data Visualization of Cleaned flight crashes dataset ')
-    ]
-)
-#contnue setting up...
+app.layout = html.Div(children=[html.H1('Flight Crashes Visualization', 
+                                        style={'textAlign':'center'}
+                                        ),
+                                dcc.Dropdown(id='years-dropdown',
+                                options=[{'label':'All years', 'value':'All'}],
+                                value = "ALL",
+                                placeholder = "Select a year",
+                                searchable=True
+                                )
+
+
+                    ])
+
+
 
 if __name__ == '__main__':
     app.run_server()

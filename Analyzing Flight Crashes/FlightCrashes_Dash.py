672373127e7843ed96ca0e62154af426d6bf1704
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2024-03-18 22:15:35 +0000

    :hatching_chick: Setup Dashboard

--- Analyzing Flight Crashes/FlightCrashes_Dash.py
+++ Analyzing Flight Crashes/FlightCrashes_Dash.py
@@ -5,22 +5,18 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 flightCrashes_data = pd.read_csv('AbdessamadTzn_flightCrashes_cleanedData.csv', encoding='ISO-8859-1')
-
+#create app
 app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[html.H1('Flight Crashes Visualization', 
+#Layout Section of Dash
+app.layout = html.Div(children=[html.H1('Flight Crashes Dashboard', 
                                         style={'textAlign':'center'}
-                                        ),
-                                dcc.Dropdown(id='years-dropdown',
-                                options=[{'label':'All years', 'value':'All'}],
-                                value = "ALL",
-                                placeholder = "Select a year",
-                                searchable=True
-                                )
+                                        )
+
 
 
                     ])
-
+#Layout ends
 
 
 if __name__ == '__main__':
